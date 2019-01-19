904a27946bbce2aa8e30e0d294abbdef3da10538
Marcin Karski <karski80@gmail.com>
2019-01-19 18:11:09 +0000

    Further implementation

--- ContainerViewControllers/ViewController.swift
+++ ContainerViewControllers/ViewController.swift
@@ -10,21 +10,29 @@ class ViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .white
         self.title = "Container View Controllers"
+        let child = LoadingViewController()
+        parent?.addChild(child)
     }
 }
 
 class LoadingViewController: UIViewController {
-    private lazy var activityIndicator = UIActivityIndicatorView(style: .gray)
-}
-
-class ErrorViewController: UIViewController {
-    private lazy var errorLabel = UILabel()
-}
-
-class ProductContentViewController: UIViewController {
-    private lazy var productView = ProductView()
     
-    class ProductView: UIView {
+    private lazy var activityIndicator = UIActivityIndicatorView(style: .gray)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
         
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+        NSLayoutConstraint.activate([activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.activityIndicator.startAnimating()
+        }
     }
 }
